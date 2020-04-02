a89be29b7df2f432b1184a1b14b07f52e922dc9f
selimslab <hselimozturk@gmail.com>
2020-04-02 12:11:14 +0300

    Fix visitor

--- api/visitor.py
+++ api/visitor.py
@@ -7,7 +7,7 @@ from spiders.visitor import run_spiders_simultaneously
 def visit_all():
     all_spiders = get_collectors()
     print(len(all_spiders), "spiders", all_spiders.keys())
-    # run_spiders_simultaneously(all_spiders)
+    run_spiders_simultaneously(all_spiders)
 
 
 def debug_visit_all():
