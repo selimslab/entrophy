4f05774abf1889e4fecd2048ceb57e51b38e2cea
selimslab <hselimozturk@gmail.com>
2020-04-09 15:39:47 +0300

    Remove report decorator

--- api/refresher.py
+++ api/refresher.py
@@ -1,20 +1,17 @@
 import data_services
-import supermatch
-from api.report_decorator import report_decorator
+from supermatch.main import create_matching
 from supersync.syncer import sync_the_new_matching
 import sentry_sdk
 sentry_sdk.init("https://39fd5a66307d47dcb3e9c37a8b709c44@sentry.io/5186400")
 
 
-
-@report_decorator
 def create_new_matching():
     docs_to_match = data_services.get_docs_to_match({})
     links_of_products: set = data_services.get_links_of_products()
-    matching_collection = supermatch.create_matching(
+    skus = create_matching(
         docs_to_match=docs_to_match, links_of_products=links_of_products
     )
-    sync_the_new_matching(matching_collection)
+    sync_the_new_matching(skus)
 
 
 if __name__ == "__main__":
