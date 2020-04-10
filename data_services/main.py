e63f8b4a9c83b258f472c891f534608bb5f5f9d3
selimslab <hselimozturk@gmail.com>
2020-04-10 18:21:35 +0300

    Clean Mongo

--- data_services/main.py
+++ data_services/main.py
@@ -1,9 +1,7 @@
 from bson import ObjectId
 import constants as keys
 import data_services.mongo.collections as collections
-from data_services.elastic.main import Elastic
 from data_services.mongo.mongo_sync import MongoSync
-from tqdm import tqdm
 
 from data_services.query_elastic import search_elastic_by_ids
 
@@ -39,5 +37,6 @@ def sync_sku_and_product_ids(id_tree):
 
 
 if __name__ == "__main__":
-    search_elastic_by_ids(["5e54cfc2d1e09b159549e7e3", "5e11bd9c1b07cf6bf3b913dd", "5d7bdfa6525e36c343df0d8c",
+    search_elastic_by_ids(["5e54cfc2d1e09b159549e7e3", "5e11bd9c1b07cf6bf3b913dd",
+                           "5d7bdfa6525e36c343df0d8c",
                            "5d7bdfa6525e36c343df0e4e"])
--- data_services/mongo/connect.py
+++ data_services/mongo/connect.py
@@ -1,16 +1,8 @@
 from pymongo import MongoClient
 
-"""
-from dotenv import load_dotenv
-import os
-load_dotenv()
-CONNECTION_STRING = os.environ.get("CONNECTION_STRING")
-"""
-
 CONNECTION_STRING = "mongodb+srv://selim:selimslab@market-8lznc.mongodb.net/test?retryWrites=true&w=majority"
 client = MongoClient(CONNECTION_STRING)
 db = client.narmoni
-
 
 backup_client = MongoClient(
     "mongodb+srv://selim:3924boun@cluster0-ztyl6.mongodb.net/test?retryWrites=true&w=majority"
