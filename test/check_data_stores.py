d565e6ed663d0cd7342ea3091491e81a088a47a5
selimslab <hselimozturk@gmail.com>
2020-04-20 13:56:45 +0300

    simplify tests

--- test/check_data_stores.py
+++ test/check_data_stores.py
@@ -1,5 +1,5 @@
 from data_services.firebase.connect import test_collection, skus_collection
-from data_services.elastic.query_elastic import search_by_ids
+from data_services.elastic.query_elastic import search_by_ids, search_elastic
 
 from data_services.mongo.collections import items_collection
 from bson import ObjectId
@@ -26,6 +26,27 @@ def check_mongo():
     doc = items_collection.find_one({"link": link})
 
     print(doc)
+
+
+def ask(collection, query):
+    cursor = collection.find(query)
+    print(collection.count_documents(query))
+    for doc in cursor[:10]:
+        pprint(doc)
+
+
+def inspect_case(link):
+    query = {keys.LINK: link}
+
+    doc = items_collection.find_one(query)
+    pprint(doc)
+    if not doc:
+        return
+
+    sku_id = doc.get(keys.SKU_ID)
+    name = doc.get(keys.NAME)
+    print(name, sku_id)
+    search_elastic(name)
 
 
 if __name__ == "__main__":
