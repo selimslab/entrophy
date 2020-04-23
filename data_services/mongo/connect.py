04c7a1c6493c7abfd03403d9d39ffd370bcf7df9
selimslab <hselimozturk@gmail.com>
2020-04-23 17:07:43 +0300

    migrate mongo

--- data_services/mongo/connect.py
+++ data_services/mongo/connect.py
@@ -1,11 +1,12 @@
 from pymongo import MongoClient
 
+"""
 CONNECTION_STRING = "mongodb+srv://selim:selimslab@market-8lznc.mongodb.net/test?retryWrites=true&w=majority"
 client = MongoClient(CONNECTION_STRING)
 db = client.narmoni
+"""
 
 NEWDB = "mongodb+srv://selim:e6nanmCqhMwOQq8q@cluster1-gx1ov.mongodb.net/test?retryWrites=true&w=majority"
 newclient = MongoClient(NEWDB)
-
-newdb = newclient.Cluster1
+db = newclient.Cluster1
 
