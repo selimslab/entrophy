12320d5e72a322b9e19daaf541c9e81d51e634b7
selimslab <hselimozturk@gmail.com>
2020-04-23 17:22:37 +0300

    unique links

--- data_services/mongo/collections.py
+++ data_services/mongo/collections.py
@@ -1,4 +1,4 @@
-from .connect import db
+from data_services.mongo.connect import db
 
 items_collection = db["raw_products"]
 
@@ -7,3 +7,5 @@ items_backup = db["raw_products_backup"]
 test_collection = db["test"]
 
 elastic_backup = db["elastic_backup"]
+
+# items_collection.create_index("link", unique=True)
