d99a8d47a7f58f47fd7e963c5a8da668dc5ee554
selimslab <hselimozturk@gmail.com>
2020-05-14 15:28:21 +0300

    fix: convertor

--- services/convertor.py
+++ services/convertor.py
@@ -3,11 +3,14 @@ from typing import Union
 
 
 def convert_price(price: str) -> Union[int, float, None]:
+    price = str(price).lower().replace("tl", "").replace("₺", "").replace("try", "").replace(" ", "").strip()
+
     dot_split = str(price).split(".")
     if len(dot_split) == 3:
         price = ".".join(dot_split[:2])
     if not price:
         return
+
     try:
         price = float(price)
     except (ValueError, TypeError) as e:
