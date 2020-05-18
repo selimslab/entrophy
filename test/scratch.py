06a4a5965f58b8098db1fc69e6f897f2b32e0488
selimslab <hselimozturk@gmail.com>
2020-05-18 19:59:36 +0300

    test: add tests

---
+++ test/scratch.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass, field
+
+
+@dataclass
+class C:
+    a: float
+    b: float
+    c: []
+
+    def __post_init__(self):
+        self.c = [c for c in self.c if c]
+
+
+x = C(a=3, b=4, c=[0, 7, 3])
+
+print(x)
+
+
+def foo(d, l):
+    d[3][2] = 8
+    l.append(5)
+
+
+def bar():
+    d = {3: {2: 4}}
+    l = []
+    foo(d, l)
+    print(d, l)
+
+
+if __name__ == "__main__":
+    pass
--- test/test_cat.py
+++ test/test_cat.py
@@ -36,3 +36,4 @@ def test_cat():
     pprint(cat_freq)
 
     pprint(cat_token_freq)
+
