02f38557e62fa8277487e2c4d4bf040fbdbf3896
Omidkk <omidk@sabanciuniv.edu>
2020-05-18 13:58:17 +0200

    - gittigidiyor crawling

--- spiders/spider_modules/visible/gittigidiyor/gittigifiyor_list.py
+++ spiders/spider_modules/visible/gittigidiyor/gittigifiyor_list.py
@@ -1,62 +1,69 @@
 import scrapy
+from bs4 import BeautifulSoup
 
 import constants as keys
-from data_services import mark_out_of_stock
 from spiders.spider_modules.base import BaseSpider
-from spiders.spider_modules.visible.with_search.a101_helper import A101Helper
 from spiders.test_spider import debug_spider
 
 
 class GittigidiyorSpider(BaseSpider):
-    name = keys.A101
+    name = keys.GITTIGIFIYOR
 
     def __init__(self, *args, **kwargs):
         super(GittigidiyorSpider, self).__init__(*args, base_domain="gittigidiyor.com/")
-        if kwargs.get("debug"):
-            print('yes')
-            self.start_urls = {"/kozmetik-kisisel-bakim/sampuan/"}
-        else:
-            print('no')
+        self.start_urls = {"supermarket", "/kozmetik-kisisel-bakim"}
 
-            self.start_urls = A101Helper.get_category_urls()
+    def start_requests(self):
+        for url in self.start_urls:
+            page_url = self.base_url + url
+            yield scrapy.Request(
+                page_url,
+                callback=self.parse,
+                meta={"url": page_url, keys.PAGE_NUMBER: 1},
+            )
 
-    # def start_requests(self):
-    #     for url in self.start_urls:
-    #         page_url = self.base_url + url
-    #         yield scrapy.Request(
-    #             page_url,
-    #             callback=self.parse,
-    #             meta={"url": page_url, keys.PAGE_NUMBER: 1},
-    #         )
-    #
-    # def parse(self, response):
-    #     table = response.css(".product-list-general")
-    #     products = table.css(".set-product-item")
-    #
-    #     for product_div in products:
-    #         product = A101Helper.extract_product_info(product_div, self.base_url)
-    #         if product:
-    #             self.links_seen.add(product.get(keys.LINK))
-    #             yield product
-    #
-    #     if self.next_page:
-    #         current_page_number = response.meta.get(keys.PAGE_NUMBER, 1)
-    #         next_page_number = current_page_number + 1
-    #         max_page_number = self.get_max_page_number(response)
-    #
-    #         if next_page_number <= max_page_number:
-    #             url = response.meta.get("url")
-    #             next_page_url = url + "?page=%s" % next_page_number
-    #             yield response.follow(
-    #                 next_page_url,
-    #                 meta={
-    #                     "url": url,
-    #                     "next_page_url": next_page_url,
-    #                     keys.PAGE_NUMBER: next_page_number,
-    #                 },
-    #                 callback=self.parse,
-    #             )
-    #
+    def parse(self, response):
+        html_body = BeautifulSoup(response.text, "html.parser")
+        parsed_html = html_body.find("ul", class_="catalog-view clearfix products-container")
+        products = parsed_html.findAll("a")
+
+        for product in products:
+            product_name = product['title']
+            url = product['href']
+            src = product.find("img", class_="img-cont")['src']
+            price = ((product.find("p", class_="fiyat robotobold price-txt")).text).replace(".", "").replace(",",
+                                                                                                             ".").replace(
+                "TL", "").strip()
+            p = {
+                keys.LINK: url,
+                keys.NAME: product_name,
+                keys.SRC: src,
+                keys.PRICE: price,
+                keys.MARKET: keys.GITTIGIFIYOR,
+                keys.OUT_OF_STOCK: False,
+            }
+            self.links_seen.add(p.get(keys.LINK))
+            yield p
+            print(p)
+
+        # if self.next_page:
+        #     current_page_number = response.meta.get(keys.PAGE_NUMBER, 1)
+        #     next_page_number = current_page_number + 1
+        #     max_page_number = self.get_max_page_number(response)
+        #
+        #     if next_page_number <= max_page_number:
+        #         url = response.meta.get("url")
+        #         next_page_url = url + "?page=%s" % next_page_number
+        #         yield response.follow(
+        #             next_page_url,
+        #             meta={
+        #                 "url": url,
+        #                 "next_page_url": next_page_url,
+        #                 keys.PAGE_NUMBER: next_page_number,
+        #             },
+        #             callback=self.parse,
+        #         )
+
     # @staticmethod
     # def get_max_page_number(response):
     #     nav_items = response.css(".pagination").css("ul li")
