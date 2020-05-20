acd4c568d2697068843dbe05945f4a85ed6670bb
Omidkk <omidk@sabanciuniv.edu>
2020-05-20 08:25:49 +0200

    - n11 fix issues

---
+++ spiders/spider_modules/visible/n11/n11_helper.py
@@ -0,0 +1,50 @@
+import requests
+from bs4 import BeautifulSoup
+
+import constants as keys
+
+
+class N11Helper:
+    @staticmethod
+    def extract_product_info(response) -> dict:
+        html_body = BeautifulSoup(str(response.text), "html.parser")
+        parsed_detail = html_body.findAll("div", class_="pro")
+        parsed_price = html_body.findAll("ins")
+        p = dict()
+        for product_div, price in zip(parsed_detail, parsed_price):
+            product = product_div.find('a')
+            product_name = product['title']
+            url = product['href']
+            src = product_div.find("img")['data-original']
+            price = price.text \
+                .replace(".", "") \
+                .replace(",", ".") \
+                .replace("TL", "") \
+                .strip()
+            p = {
+                keys.LINK: url,
+                keys.NAME: product_name,
+                keys.SRC: src,
+                keys.PRICE: price,
+                keys.MARKET: keys.N11,
+                keys.OUT_OF_STOCK: False,
+            }
+
+        return p
+
+    @staticmethod
+    def get_kozmetik_kisisel_bakim_urls(base_domain):
+        categories_url = list()
+        response = requests.get(base_domain)
+        html_body = BeautifulSoup(response.text, "html.parser")
+        parsed_html = html_body.findAll("li", class_="catMenuItem")
+        category = ''
+        for category_div in parsed_html:
+            category = category_div.find('a')
+            if "kozmetik" in category.text.lower():
+                category = category_div
+                break
+        for subcategory in category.findAll("li", class_="subCatMenuItem"):
+            categories_url.append('/' + ((subcategory.find('a')['href']).split('/'))[-1])
+
+        return categories_url
--- spiders/spider_modules/visible/n11/n11_list.py
+++ spiders/spider_modules/visible/n11/n11_list.py
@@ -6,30 +6,16 @@ import constants as keys
 from data_services import mark_out_of_stock
 from spiders.spider_modules.base import BaseSpider
 from spiders.test_spider import debug_spider
-
+from spiders.spider_modules.visible.n11.n11_helper import N11Helper
 
 class N11Spider(BaseSpider):
     name = keys.N11
 
     def __init__(self, *args, **kwargs):
         super(N11Spider, self).__init__(*args, base_domain="n11.com")
-        self.start_urls = ['/supermarket'] + self.get_kozmetik_kisisel_bakim_urls(base_domain=self.base_url)
+        self.start_urls = ['/supermarket'] + N11Helper.get_kozmetik_kisisel_bakim_urls(base_domain=self.base_url)
 
-    def get_kozmetik_kisisel_bakim_urls(self, base_domain):
-        categories_url = list()
-        response = requests.get(base_domain)
-        html_body = BeautifulSoup(response.text, "html.parser")
-        parsed_html = html_body.findAll("li", class_="catMenuItem")
-        category = ''
-        for category_div in parsed_html:
-            category = category_div.find('a')
-            if "kozmetik" in category.text.lower():
-                category = category_div
-                break
-        for subcategory in category.findAll("li", class_="subCatMenuItem"):
-            categories_url.append('/' + ((subcategory.find('a')['href']).split('/'))[-1])
 
-        return categories_url
 
     def start_requests(self):
         for url in self.start_urls:
@@ -41,29 +27,9 @@ class N11Spider(BaseSpider):
             )
 
     def parse(self, response):
-        html_body = BeautifulSoup(str(response.text), "html.parser")
-        parsed_detail = html_body.findAll("div", class_="pro")
-        parsed_price = html_body.findAll("ins")
-        for product_div, price in zip(parsed_detail, parsed_price):
-            product = product_div.find('a')
-            product_name = product['title']
-            url = product['href']
-            src = product_div.find("img")['data-original']
-            price = price.text \
-                .replace(".", "") \
-                .replace(",", ".") \
-                .replace("TL", "") \
-                .strip()
-            p = {
-                keys.LINK: url,
-                keys.NAME: product_name,
-                keys.SRC: src,
-                keys.PRICE: price,
-                keys.MARKET: keys.N11,
-                keys.OUT_OF_STOCK: False,
-            }
-            self.links_seen.add(p.get(keys.LINK))
-            yield p
+        product = N11Helper.extract_product_info(response)
+        self.links_seen.add(product.get(keys.LINK))
+        yield product
 
         self.next_page = self.check_next_page(response)
         if self.next_page:
