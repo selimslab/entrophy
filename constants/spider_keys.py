861b41a8ae68577f5d7881dabb33443e5459a9a7
selimslab <hselimozturk@gmail.com>
2020-05-29 18:20:18 +0300

    feat: add spider keys

--- constants/spider_keys.py
+++ constants/spider_keys.py
@@ -1,13 +1,23 @@
 # spider keys
 NAME = "name"
+CLEAN_NAMES = "clean_names"
 BARCODES = "barcodes"
 MARKET = "market"
 PRICE = "price"
 LINK = "link"
 SRC = "src"
+
 BRAND = "brand"
+BRAND_CANDIDATES = "brand_candidates"
+BRANDS_MULTIPLE = "brands"
+
+CLEAN_BRANDS = "clean_brands"
+
 CATEGORIES = "categories"
+CLEAN_CATS = "clean_cats"
+
 SUB_CATEGORIES = "sub_categories"
+CLEAN_SUBCATS = "clean_subcats"
 
 OUT_OF_STOCK = "out_of_stock"
 
