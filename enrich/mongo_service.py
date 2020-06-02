54c468ea8a07b229927490e017a23f56927a7a0c
selimslab <hselimozturk@gmail.com>
2020-06-02 12:23:41 +0300

    feat: add new mongo service

---
+++ enrich/mongo_service.py
@@ -0,0 +1,30 @@
+import constants as keys
+
+
+def get_raw_docs_with_markets_and_cats_only():
+    from data_services.mongo.collections import items_collection
+    cursor = items_collection.find(
+        {
+            keys.MARKET: {"$exists": True},
+            keys.CATEGORIES: {"$exists": True},
+            keys.MARKETS: {"$nin": [keys.TRENDYOL, keys.WATSONS]},
+        },
+        {keys.MARKET: 1, keys.CATEGORIES: 1, keys.BRAND: 1, "_id": 0},
+    )
+    return cursor
+
+
+def ask_a_question():
+    from data_services.mongo.collections import items_collection
+    from pprint import pprint
+    cursor = items_collection.find(
+        {
+            keys.BRAND: {"$in": ["Elixir", "Limon"]},
+        },
+    )
+    for doc in cursor:
+        pprint(doc)
+
+
+if __name__ == "__main__":
+    ...
