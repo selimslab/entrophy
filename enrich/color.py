0859530de6cd75981c039def19c9e83d9fd74275
selimslab <hselimozturk@gmail.com>
2020-06-09 15:39:22 +0300

    update old color script

--- enrich/color.py
+++ enrich/color.py
@@ -26,14 +26,15 @@ def get_all_colors():
 
 
 def clean_colors(colors):
-    clean_colors = services.list_to_clean_set(colors)
-    clean_colors = [c for c in clean_colors if not c.isdigit() and "nocolor" not in c]
+    clean_colors = [services.clean_name(c) for c in colors]
+    clean_colors = [c for c in clean_colors if c and not c.isdigit() and "nocolor" not in c]
+    clean_colors = sorted(list(set(clean_colors)))
+    # letters_only = re.compile(r"[^a-z]")
+    # re.sub(letters_only, "", t)
+    return clean_colors
 
-    letters_only = re.compile(r"[^a-z]")
 
-    clean_colors = [
-        " ".join(re.sub(letters_only, "", t).strip() for t in color.split()).strip()
-        for color in clean_colors
-    ]
-    clean_colors = list(set(clean_colors))
+if __name__ == "__main__":
+    colors = get_all_colors()
+    clean_colors = clean_colors(colors)
     services.save_json(output_dir / "clean_colors.json", clean_colors)
