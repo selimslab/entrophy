9c3d49a20d95bad1107bc0d05e74820b1bf6be36
selimslab <hselimozturk@gmail.com>
2020-06-09 15:39:37 +0300

    add: filter all products

--- enrich/filter_names.py
+++ enrich/filter_names.py
@@ -1,5 +1,5 @@
 import os
-from collections import Counter
+from collections import Counter, OrderedDict, defaultdict
 import logging
 from typing import List
 import itertools
@@ -35,22 +35,14 @@ color = {
     "yesil",
 }
 
-stopwords = {"ve", "ile", "for", "için", "veya", "li", "lu", "ml", "gr", "kg", "lt"}
+stopwords = {"ve", "ile", "for", "icin", "veya", "li", "lu", "ml", "gr", "kg", "lt"}
 
 
-def remove_brand(name: str, brands: List[str]):
-    for brand in brands:
-        if brand in name:
-            name = name.replace(brand, "")
-
-    return name
-
-
-def remove_subcat(name: str, subcats: List[str]):
-    for sub in subcats:
-        if sub in name:
-            name = name.replace(sub, "")
-    return name
+def remove_a_list_of_strings(s: str, to_remove: list):
+    for bad in to_remove:
+        if bad in s:
+            s = s.replace(bad, "")
+    return s
 
 
 def is_barcode(s: str):
@@ -86,7 +78,8 @@ def is_known_token(s: str):
 
 def filter_tokens(name: str):
     tokens = name.split()
-    filtered_tokens = [t for t in tokens if not is_known_token(t) and len(t) > 1]
+    filtered_tokens = [t.strip() for t in tokens if not is_known_token(t)]
+    filtered_tokens = [t for t in filtered_tokens if len(t) > 1 and t.isalnum()]
     return " ".join(filtered_tokens)
 
 
@@ -97,7 +90,7 @@ def remove_size(name: str):
     return name
 
 
-def filter_out_knownword_groups_from_a_name(product):
+def filter_out_knownword_groups_from_a_name(product, clean_colors):
     clean_names = product.get(keys.CLEAN_NAMES)
     brand_candidates = product.get(keys.BRAND_CANDIDATES)
     subcat_candidates = product.get(keys.SUBCAT_CANDIDATES)
@@ -108,8 +101,9 @@ def filter_out_knownword_groups_from_a_name(product):
     filtered_names = []
     for name in clean_names:
         name = remove_size(name)
-        name = remove_brand(name, sorted_brands)
-        name = remove_subcat(name, sorted_subcats)
+        name = remove_a_list_of_strings(name, sorted_brands)
+        name = remove_a_list_of_strings(name, sorted_subcats)
+        name = remove_a_list_of_strings(name, clean_colors)
         name = filter_tokens(name)
         if name:
             filtered_names.append(name)
@@ -121,8 +115,13 @@ def filter_all_products():
     products = services.read_json(
         output_dir / "products_with_brand_and_sub_cat.json"
     )
+
+    clean_colors = services.read_json(output_dir / "clean_colors.json")
+    clean_colors = sorted(list(clean_colors), key=len, reverse=True)
+
     for product in tqdm(products):
-        filtered_names = filter_out_knownword_groups_from_a_name(product)
+        filtered_names = filter_out_knownword_groups_from_a_name(product, clean_colors)
+        filtered_names = Counter(filtered_names)
         print(filtered_names)
         product["filtered_names"] = filtered_names
 
@@ -132,5 +131,41 @@ def filter_all_products():
     )
 
 
+def create_sub_brand_tree(products_filtered):
+    """cat: { sub_cat : { type: {brand: {sub_brand : [products] } }"""
+
+    tree = {}
+    logging.info("create_sub_brand_tree..")
+
+    for product in tqdm(products_filtered):
+        brand, subcat = product.get(keys.BRAND), product.get(keys.SUBCAT)
+
+        if not (brand and subcat):
+            continue
+
+        filtered_names = product.get("filtered_names")
+        if not filtered_names:
+            continue
+
+        if subcat not in tree:
+            tree[subcat] = {}
+        if brand not in tree[subcat]:
+            tree[subcat][brand] = []
+
+        tree[subcat][brand].append(filtered_names)
+
+    return tree
+
+
+def save_sub_tree():
+    products = services.read_json(
+        output_dir / "products_filtered.json",
+    )
+    sub_brand_tree = create_sub_brand_tree(products)
+    services.save_json(output_dir / "sub_brand_tree.json", sub_brand_tree)
+
+
 if __name__ == "__main__":
-    filter_all_products()
+    # filter_all_products()
+    # save_sub_tree()
+    ...
