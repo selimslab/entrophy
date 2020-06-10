ba5b3cb149137112b46561acc645884e9f4535fb
selimslab <hselimozturk@gmail.com>
2020-06-10 11:45:06 +0300

    fix to_excel

--- enrich/to_excel.py
+++ enrich/to_excel.py
@@ -1,5 +1,7 @@
 from tqdm import tqdm
-
+from pprint import pprint
+import logging
+from collections import defaultdict
 import services
 from paths import output_dir, input_dir
 
@@ -14,16 +16,41 @@ def add_sku_id_and_product_id_to_pairs():
     products_with_brand_and_sub_cat = services.read_json(
         output_dir / "products_with_brand_and_sub_cat.json"
     )
-    for sku in tqdm(products_with_brand_and_sub_cat):
-        sku_id = sku.get("sku_id")
+
+    logging.info("pid_tree..")
+    pid_tree = defaultdict(set)
+    for sku in skus.values():
+        sku_id = sku.get(keys.SKU_ID)
+        pid = sku.get(keys.PRODUCT_ID)
+        if pid and sku_id:
+            pid_tree[pid].add(sku_id)
+
+    def add_brand_and_subcat_to_doc(sku_id):
+        sku = skus.get(sku_id, {})
         product_id = sku.get(keys.PRODUCT_ID)
-        doc_ids = skus.get(sku_id, {}).get(keys.DOC_IDS, [])
+
+        doc_ids = sku.get(keys.DOC_IDS, [])
         if doc_ids:
             for doc_id in doc_ids:
-                pairs[doc_id]["our_brand"] = sku.get(keys.BRAND)
-                pairs[doc_id][keys.SUBCAT] = sku.get(keys.SUBCAT)
-                pairs[doc_id][keys.PRODUCT_ID] = product_id
-                pairs[doc_id][keys.SKU_ID] = sku_id
+                doc = pairs[doc_id]
+                doc["our_brand"] = product_with_brand_and_sub.get(keys.BRAND)
+                doc[keys.SUBCAT] = product_with_brand_and_sub.get(keys.SUBCAT)
+                doc[keys.PRODUCT_ID] = product_id
+                doc[keys.SKU_ID] = sku_id
+                pairs[doc_id] = doc
+
+    logging.info("add_brand_and_subcat_to_doc..")
+    for product_with_brand_and_sub in tqdm(products_with_brand_and_sub_cat):
+        sku_id = product_with_brand_and_sub.get(keys.SKU_ID)
+
+        if sku_id:
+            add_brand_and_subcat_to_doc(sku_id)
+        else:
+            product_id = product_with_brand_and_sub.get(keys.PRODUCT_ID)
+            sku_ids = pid_tree.get(product_id, [])
+            for sku_id in sku_ids:
+                add_brand_and_subcat_to_doc(sku_id)
+
     return pairs
 
 
@@ -35,3 +62,5 @@ def to_excel():
 
 if __name__ == "__main__":
     to_excel()
+    # pairs = add_sku_id_and_product_id_to_pairs()
+
