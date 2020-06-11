a41cce539d9763f669a8a911a0fa6a9f99618f81
selimslab <hselimozturk@gmail.com>
2020-06-11 19:30:50 +0300

    refactor branding

--- enricher/branding.py
+++ enricher/branding.py
@@ -43,19 +43,18 @@ def get_brand_pool(products: List[dict], possible_subcats_by_brand: dict) -> set
 
     most_frequent_start_strings = {
         s for s, count in window_frequencies.items()
-        if len(s) > 2 and count > 42
+        if count > 42
     }
 
     # OrderedDict(Counter(groups).most_common())
     brand_pool.update(most_frequent_start_strings)
 
-    to_filter_out = {"brn ", "markasiz", "erkek", "kadin"}
+    to_filter_out = {"brn ", "markasiz", "erkek", "kadin", " adet"}
     brand_pool = {
-        b
+        services.remove_whitespace(b)
         for b in brand_pool
         if (len(b) > 2 and not any(bad in b for bad in to_filter_out))
     }
-
     return brand_pool
 
 
@@ -90,7 +89,7 @@ def get_brand_candidates(
     to_partial_search = set()
     for name in clean_names:
         # brand is in first 4 tokens mostly
-        start_strings = services.string_to_extending_windows(name, 4)
+        start_strings = services.string_sliding_windows(name)
         for s in start_strings:
             if s in brand_pool:
                 brand_candidates.add(s)
