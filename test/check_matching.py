6b0e9202af36a725c09ccb6721263f9893f4207b
selimslab <hselimozturk@gmail.com>
2020-06-12 14:07:55 +0300

    refactor matching test

--- test/check_matching.py
+++ test/check_matching.py
@@ -17,18 +17,26 @@ from pprint import pprint
 import pathlib
 
 cwd = pathlib.Path.cwd()
-logs_dir = cwd / "logs"
+input_dir = cwd / "in"
+output_dir = cwd / "out"
+
+raw_docs_path = input_dir / "raw_docs_path.json",
+pairs_path = input_dir / "pairs.json",
+skus_path = output_dir / "skus.json"
+
+
+def match_and_save(pairs):
+    skus: dict = create_matching(id_doc_pairs=pairs)
+    services.save_json(skus_path, skus)
 
 
 def create_new_matching_from_query():
     links = services.read_json("test_logs/old/links.json")
     query = {keys.LINK: {"$in": services.flatten(links)}}
     docs_to_match = data_services.get_docs_to_match(query)
-    pairs = preprocess.create_id_doc_pairs(docs_to_match)
-
-    services.save_json(logs_dir / "pairs.json", pairs)
-    skus: dict = create_matching(id_doc_pairs=pairs)
-    services.save_json(logs_dir / "skus.json", skus)
+    pairs = preprocess.get_clean_id_doc_pairs(docs_to_match)
+    services.save_json(pairs_path, pairs)
+    match_and_save(pairs)
 
 
 def create_new_matching_from_scratch():
@@ -36,20 +44,16 @@ def create_new_matching_from_scratch():
         {keys.MARKET: {"$in": keys.MATCHING_MARKETS}}
     )
     pairs = preprocess.get_clean_id_doc_pairs(docs_to_match)
-    services.save_json(logs_dir / "pairs.json", pairs)
-    skus: dict = create_matching(id_doc_pairs=pairs)
-    services.save_json(logs_dir / "skus.json", skus)
+    services.save_json(pairs_path, pairs)
+    match_and_save(pairs)
 
 
 def create_new_matching_from_existing_pairs():
-    pairs = services.read_json(logs_dir / "pairs.json")
-    skus: dict = create_matching(id_doc_pairs=pairs)
-    services.save_json(logs_dir / "skus.json", skus)
+    pairs = services.read_json(pairs_path)
+    match_and_save(pairs)
 
 
 if __name__ == "__main__":
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
     logging.getLogger().setLevel(logging.DEBUG)
     create_new_matching_from_existing_pairs()
-    # why ty is not included in matching ??
-    # because their newly issued sku and pid s are not added to pairs
