2f5f8a2a3e871f00133c5230db0e1f9dc7b3b9bd
selimslab <hselimozturk@gmail.com>
2020-06-19 15:33:00 +0300

    add logs

--- services/files/excel.py
+++ services/files/excel.py
@@ -6,12 +6,20 @@ from tqdm import tqdm
 
 
 def create_excel(rows: list, path, colnames: list):
-    print("creating excel..")
+    print("filtering rows..")
     items = [{k: v for k, v in doc.items() if k in set(colnames)} for doc in tqdm(rows)]
+
+    print("rows to df..")
     df = pd.DataFrame(items).fillna("")
+
+    print("filtering columns..")
     cols = [c for c in colnames if c in df.columns]
     df = df[cols]
+
+    print("sorting..")
     df = df.sort_values(keys.SKU_ID)
+
+    print("df to excel..")
     df.to_excel(path, index=False)
     print("done")
 
