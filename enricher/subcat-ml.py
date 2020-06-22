bd762033392721e65c23b0aa132efdab8914fd07
selimslab <hselimozturk@gmail.com>
2020-06-22 17:42:45 +0300

    create predict_subcat()

--- enricher/subcat-ml.py
+++ enricher/subcat-ml.py
@@ -10,23 +10,22 @@ import constants as keys
 import paths as paths
 
 from filter_names import add_filtered_names
+from main import get_indexes
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 
 
-def classify_subcat():
+def predict_subcat(products: List[dict]) -> list:
     """
     use products with subcat as training, and classify the remaining
     """
-    products = services.read_json(paths.products_out)
-    products = add_filtered_names(products, remove_subcat=False)
-    relevant_keys = {keys.FILTERED_NAMES, keys.SUBCAT, keys.CLEAN_NAMES, keys.BRAND}
-    products = [services.filter_keys(p, relevant_keys) for p in products]
+    if not products:
+        return []
 
-    with_sub = [p for p in products if keys.SUBCAT in p]
-    no_sub = [p for p in products if keys.SUBCAT not in p]
+    with_sub = [p for p in products if keys.SUBCAT in p and p.get(keys.SUBCAT_SOURCE) != "global_name"]
+    no_sub = [p for p in products if keys.SUBCAT not in p or p.get(keys.SUBCAT_SOURCE) == "global_name"]
 
     X_train = []
     y_train = []
@@ -37,14 +36,21 @@ def classify_subcat():
         X_train += [" ".join(names)]
         y_train += [sub]
 
+    filtered_names = [product.get(keys.FILTERED_NAMES, []) for product in tqdm(no_sub)]
+    X_test = [" ".join(names) for names in filtered_names]
+
+    if not X_train or not y_train or not X_test:
+        return []
+
     count_vect = CountVectorizer()
-    X_train_counts = count_vect.fit_transform(X_train)
+    try:
+        X_train_counts = count_vect.fit_transform(X_train)
+    except ValueError:
+        return []
     tfidf_transformer = TfidfTransformer()
     X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
     clf = MultinomialNB().fit(X_train_tfidf, y_train)
 
-    filtered_names = [product.get(keys.FILTERED_NAMES, []) for product in tqdm(no_sub)]
-    X_test = [" ".join(names) for names in filtered_names]
     y_pred = clf.predict(count_vect.transform(X_test))
 
     for i, product in enumerate(no_sub):
@@ -56,21 +62,32 @@ def classify_subcat():
         print(pred)
         print()
 
-    services.save_json("out/sub_ml.json", no_sub)
+    return no_sub
 
 
-def test_grouping():
-    products = services.read_json(paths.products_out)
-    # products = add_filtered_names(products, remove_subcat=False)
-    relevant_keys = {keys.FILTERED_NAMES, keys.SUBCAT, keys.CLEAN_NAMES, keys.BRAND}
-    products = [services.filter_keys(p, relevant_keys) for p in products]
-
+def group_by_brand(products):
     keyfunc = lambda product: product.get(keys.BRAND, "")
     products = sorted(products, key=keyfunc)
     for key, items in itertools.groupby(products, key=keyfunc):
-        print(key, items)
+        yield key, items
+
+
+def run():
+    products = services.read_json(paths.products_out)
+    *rest, possible_subcats_by_brand = get_indexes(products)
+    products = add_filtered_names(products, possible_subcats_by_brand, remove_subcat=False)
+    relevant_keys = {keys.PRODUCT_ID, keys.SKU_ID, keys.FILTERED_NAMES, keys.SUBCAT, keys.SUBCAT_SOURCE,
+                     keys.CLEAN_NAMES, keys.BRAND}
+    products = [services.filter_keys(p, relevant_keys) for p in products]
+
+    subcat_predicted = []
+    for brand, products in group_by_brand(products):
+        predicted = predict_subcat(list(products))
+        subcat_predicted += predicted
+
+    services.save_json("stage/ML_subcat_predicted.json", subcat_predicted)
 
 
 if __name__ == "__main__":
     logging.getLogger().setLevel(logging.DEBUG)
-    test_grouping()
+    run()
