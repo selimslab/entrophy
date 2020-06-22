eef17692b028c011c9fd63eaa51d198c79a7b76f
selimslab <hselimozturk@gmail.com>
2020-06-22 14:39:57 +0300

    filter out wrong_brands

--- enricher/original_to_clean.py
+++ enricher/original_to_clean.py
@@ -9,7 +9,7 @@ def get_brand_original_to_clean(products: List[dict]):
     logging.info("cleaning brands..")
     brand_original_to_clean = {}
     to_filter_out = {"brn ", "markasiz", "erkek", "kadin", " adet"}
-
+    wrong_brands = {"dr"}
     for product in products:
         brands = product.get(keys.BRANDS_MULTIPLE, [])
         clean_brands = []
@@ -17,6 +17,8 @@ def get_brand_original_to_clean(products: List[dict]):
             if brand not in brand_original_to_clean:
                 clean_brand = services.clean_string(brand)
                 if any(bad in brand for bad in to_filter_out):
+                    continue
+                if brand in wrong_brands:
                     continue
                 if clean_brand == "loreal":
                     clean_brand = "loreal paris"
@@ -32,7 +34,7 @@ def get_subcat_original_to_clean(products: List[dict]) -> dict:
     # filter out overly broad cats
     bads = {"indirim", "%"}
     ## TODO check
-    too_broad = {"kozmetik", "supermarket", "gida", "el", "erkek"}
+    too_broad = {"kozmetik", "supermarket", "gida", "el", "erkek", "bakim", "icecek"}
     for product in products:
         subcats = product.get(keys.SUB_CATEGORIES, [])
         clean_subcats = []
@@ -40,9 +42,9 @@ def get_subcat_original_to_clean(products: List[dict]) -> dict:
             clean_sub = services.clean_string(sub)
             clean_sub = services.plural_to_singular(clean_sub)
             if (
-                len(clean_sub) < 40
-                and not any(bad in clean_sub for bad in bads)
-                and clean_sub not in too_broad
+                    len(clean_sub) < 40
+                    and not any(bad in clean_sub for bad in bads)
+                    and clean_sub not in too_broad
             ):
                 clean_subcats.append(clean_sub)
                 subcat_original_to_clean[sub] = clean_sub
@@ -63,9 +65,9 @@ def get_color_original_to_clean(products: List[dict]) -> dict:
             if color not in color_original_to_clean:
                 clean_color = services.clean_string(color)
                 if (
-                    not clean_color
-                    or clean_color.isdigit()
-                    or any(sw in clean_color for sw in stopwords)
+                        not clean_color
+                        or clean_color.isdigit()
+                        or any(sw in clean_color for sw in stopwords)
                 ):
                     continue
                 color_original_to_clean[color] = clean_color
