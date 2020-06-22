4d458d4d2fe6992046f393ea30fccfdb7135277e
selimslab <hselimozturk@gmail.com>
2020-06-22 17:01:09 +0300

    add stages()

--- enricher/analysis.py
+++ enricher/analysis.py
@@ -1,6 +1,7 @@
 import logging
 from typing import List
 from collections import defaultdict, Counter, OrderedDict
+import itertools
 
 import services
 import constants as keys
@@ -15,10 +16,9 @@ def sort_subcat():
     )
 
 
-def analyze_subcat():
+def analyze_subcat(products):
     # which subcats are assigned ?
-    products_out = services.read_json(paths.products_out)
-    subcats_assigned = [p.get(keys.SUBCAT) for p in products_out]
+    subcats_assigned = [p.get(keys.SUBCAT) for p in products]
     subcats_assigned = services.flatten(subcats_assigned)
     # subcats_assigned = list(set(subcats_assigned))
 
@@ -27,26 +27,36 @@ def analyze_subcat():
         "out/subcats_assigned.json",
         OrderedDict(Counter(subcats_assigned).most_common()),
     )
+    return subcats_assigned
 
 
-def analyze_brand():
+def analyze_brand(products):
     # which brands are assigned ?
-    products_out = services.read_json(paths.products_out)
-    brands_assigned = [p.get(keys.BRAND) for p in products_out]
+    brands_assigned = [p.get(keys.BRAND) for p in products]
     brands_assigned = services.flatten(brands_assigned)
     # by which freq
     services.save_json(
         "out/brands_assigned.json", OrderedDict(Counter(brands_assigned).most_common())
     )
     # with_brand_only = services.read_json(paths.output_dir / "with_brand_only.json")
+    return brands_assigned
 
 
-def how_many_with_vendor_brand():
-    products_out = services.read_json(paths.products_out)
-    with_vendor_brand = sum(1 if p.get("brands") else 0 for p in products_out)
+def how_many_with_vendor_brand(products):
+    with_vendor_brand = sum(1 if p.get("brands") else 0 for p in products)
     print(with_vendor_brand)
 
 
+def stages(products):
+    relevant_keys = {keys.PRODUCT_ID, keys.SKU_ID, keys.SUBCAT, keys.CLEAN_NAMES, keys.BRAND, keys.SUBCAT_SOURCE}
+    products = [services.filter_keys(p, relevant_keys) for p in products]
+    keyfunc = lambda product: product.get(keys.SUBCAT_SOURCE, "")
+    products = sorted(products, key=keyfunc)
+    for key, items in itertools.groupby(products, key=keyfunc):
+        services.save_json("stage/" + key + ".json", list(items))
+
+
 if __name__ == "__main__":
-    analyze_brand()
-    analyze_subcat()
+    products = services.read_json(paths.products_out)
+
+    stages()
