47d9b280364898394c8177db6f6af75e60aa6f57
selimslab <hselimozturk@gmail.com>
2020-06-26 10:17:46 +0300

    add comments

--- api/refresher.py
+++ api/refresher.py
@@ -12,12 +12,19 @@ sentry_sdk.init("https://39fd5a66307d47dcb3e9c37a8b709c44@sentry.io/5186400")
 
 
 def create_new_matching():
+    # prep
     docs_to_match = data_services.get_docs_to_match(
         {keys.MARKET: {"$in": keys.MATCHING_MARKETS}}
     )
     pairs = get_clean_id_doc_pairs(docs_to_match)
+
+    # match
     skus: dict = create_matching(id_doc_pairs=pairs)
+
+    # enrich
     skus, _ = add_brand_sub_brand_subcat_to_skus(skus)
+
+    # sync
     syncer = Syncer(debug=False)
     syncer.sync_the_new_matching(skus)
 
